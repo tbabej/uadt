8c1af1e4542edad771230602824a3d656883ae34
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:38:26 +0100

    automation: Start appium in a infinite loop

--- automation/run_appium.sh
+++ automation/run_appium.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/bash
 source config.sh
 
-# Run appium only if appium-doctor checks pass
-if [[ ! -z `appium-doctor 2>&1 | tee /dev/stderr | grep 'Everything looks good'` ]]
-then
-  appium
-fi
+start_appium () {
+  APPIUM_PID=`ps a | grep node | grep appium | cut -f1 -d' '`
+  if [[ ! -z $APPIUM_PID ]]
+  then
+    # Appium is hard to terminate
+    kill -9 $APPIUM_PID
+    sleep 3
+    kill -9 $APPIUM_PID
+    sleep 3
+  fi
+
+  # Run appium only if appium-doctor checks pass
+  if [[ ! -z `appium-doctor 2>&1 | tee /dev/stderr | grep 'Everything looks good'` ]]
+  then
+    appium
+  fi
+}
+
+while true
+do
+  start_appium
+done
