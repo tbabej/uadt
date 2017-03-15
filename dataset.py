c4709496f4177b78a8a7b5a2c189c22f3375ca27
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:37:01 +0100

    replicate: dataset: Refactor the data generation script

--- dataset.py
+++ dataset.py
@@ -1,35 +1,72 @@
 """
-Generates the training / testing datasets.
+Dataset - generate the dataset out of directory with pcap files.
+
+Usage:
+  dataset.py <directory> [--outfile=<filename>] [--part=<spec>]
+
+Options:
+  --outfile=<kn>  The name of output .csv file [default: <directory>.csv].
+  --part=<num>  Specifies the part of the dataset to generate, see "Parts" section.
+
+Parts:
+Part 1 represents processing of files 1..100.
+Part 2 represents processing of files 101..200.
+part n represents processing of files -1+(n-1)*100..n*100.
+
+If not first part is being processed, the outfile will be appended to.
+
+Use this option to hackishly parallelize dataset generation, by running in multiple terminals:
+$ for part in `seq 1 10`; do python dataset.py data --part $part; done
+$ for part in `seq 11 20`; do python dataset.py data --part $part; done
+$ for part in `seq 21 30`; do python dataset.py data --part $part; done
+...
+
 """
 
+from docopt import docopt
+
+import os
 import glob
 import pandas
 
 from flow import Flow
 
-import sys
 
-paths = list(sorted(glob.glob('data10000/*.pcap')))
-total_paths = len(paths)
+def main(arguments):
+    search_string = os.path.join(arguments['<directory>'], '*.pcap')
+    paths = list(sorted(glob.glob(search_string)))
+    files_count = len(paths)
 
-part = int(sys.argv[1]) * 100
-start = max(part-100, 0)
-end = min(part, total_paths)
+    part = arguments.get('--part')
 
-paths = paths[start:end]
+    # Determine the range of files to be processed
+    if part:
+        part = int(part) * 100
+        start = max(part-100, 0)
+        end = min(part, files_count)
+    else:
+        start = 0
+        end = files_count
 
-counter = start + 1
-raw_data = []
-for path in paths:
-    print '[{1}/{2}] Processing: {0}'.format(path, counter, total_paths)
-    f = Flow(path)
-    raw_data.append(f.features)
-    del f
-    counter = counter + 1
+    # Extract features from selected files
+    raw_data = []
+    for counter, path in enumerate(paths):
+        print '[{1}/{2}] Processing: {0}'.format(path, counter+1, files_count)
 
-data = pandas.DataFrame(raw_data)
+        f = Flow(path)
+        raw_data.append(f.features)
+        del f
 
-if start == 0:
-    data.to_csv('dataset10000.csv', header=True, mode='w')
-else:
-    data.to_csv('dataset10000.csv', header=False, mode='a')
+    # Write data out to file
+    data = pandas.DataFrame(raw_data)
+    filename = arguments['--outfile'] or '{0}.csv'.format(arguments['<directory>'])
+
+    if start == 0:
+        data.to_csv(filename, header=True, mode='w')
+    else:
+        data.to_csv(filename, header=False, mode='a')
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    main(arguments)
