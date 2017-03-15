6f830022042092399d2d54ba9a37e66ef764c35f
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:38:23 +0100

    Signal: Split into online and offline user part for clarity

--- automation/scenarios/signal.py
+++ automation/scenarios/signal.py
@@ -7,6 +7,7 @@ from plugins import Plugin
 ONLINE_USER = "Kram"
 OFFLINE_USER = "Babej"
 
+
 class SignalScenario(Plugin):
     """
     A testing scenario for Signal messagenger.
@@ -18,15 +19,27 @@ class SignalScenario(Plugin):
     app_activity = "org.thoughtcrime.securesms.ConversationListActivity"
 
     def run(self):
-        s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/menu_search')
+        self.perform_user_dependent_events(OFFLINE_USER, False)
+        self.perform_user_dependent_events(ONLINE_USER, True)
 
+
+    def perform_user_dependent_events(self, user, delivered):
+        """
+        Performs all the logged actions that depend on the target user status
+        (whether he's online or offline).
+        """
+
+        def delivered_suffix(event_identifier):
+            return event_identifier + ('_delivered' if delivered else '_undelivered')
+
+        s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/menu_search')
         with self.mark('click_search_event'):
             s.click()
 
         # User search
         s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/search_src_text')
         with self.mark('user_search'):
-            s.send_keys(OFFLINE_USER)
+            s.send_keys(user)
 
         # Open conversation
         s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/from')
@@ -35,15 +48,23 @@ class SignalScenario(Plugin):
 
         # Send regular message
         s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/embedded_text_editor')
-        with self.mark('send_regular_message'):
+        with self.mark(delivered_suffix('send_regular_message')):
             s.click()
             s.send_keys("This is an automated message. Please keep calm.\n")
             self.driver.find_element_by_id('org.thoughtcrime.securesms:id/send_button').click()
 
         # Send picture
         s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/attach_button')
-        with self.mark('send_image_no_caption'):
+        with self.mark(delivered_suffix('send_image_no_caption')):
             s.click()
             self.driver.find_element_by_id('org.thoughtcrime.securesms:id/thumbnail').click()
             self.driver.find_element_by_id('org.thoughtcrime.securesms:id/send_button').click()
             time.sleep(3)  # Extra sleep for transfer
+
+        # Press the back button (once to retract keyboard, once to go back)
+        self.driver.press_keycode(4)
+        time.sleep(2)
+        self.driver.press_keycode(4)
+
+        s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/search_close_btn')
+        s.click()
