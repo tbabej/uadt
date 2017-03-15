4d0a16049932d00997bc9535578bddc8825c1409
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:38:01 +0100

    plugins: Extend capture mechanism with timeouts

--- automation/plugins.py
+++ automation/plugins.py
@@ -1,8 +1,9 @@
 import datetime
 import contextlib
+import os
 import subprocess
 import shlex
-import os
+import time
 
 from logger import LoggerMixin
 
@@ -22,7 +23,17 @@ class Plugin(LoggerMixin):
     __metaclass__ = PluginMount
 
     @contextlib.contextmanager
-    def capture(self, name):
+    def capture(self, name, timeout=5):
+        """
+        Captures network traffic that passes the network inteface while the
+        yielded block of code is executed + while the timeout expires.
+
+        Arguments:
+          name: specify the name of the event, used to identify output file
+          timeout: the number of seconds we should wait (and capture) after the
+                   action has been performed
+        """
+
         filename = '{plugin_identifier}_{name}_{timestamp}.pcap'.format(**{
             'plugin_identifier': self.identifier,
             'name': name,
@@ -34,4 +45,7 @@ class Plugin(LoggerMixin):
         with open(os.devnull, 'w') as f:
             p = subprocess.Popen(args, stdout=f, stderr=f)
             yield
+            # Sleep so that network communication associated with the given
+            # action has time to happen
+            time.sleep(timeout)
             p.terminate()
