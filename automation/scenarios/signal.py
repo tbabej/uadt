b3ae12fa782fd5e944a2945711b25df38deaa9e8
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:38:18 +0100

    scenarios: Port signal scenario to marks

--- automation/scenarios/signal.py
+++ automation/scenarios/signal.py
@@ -15,22 +15,22 @@ class SignalScenario(Plugin):
     def run(self):
         s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/menu_search')
 
-        with self.capture('click_search_event'):
+        with self.mark('click_search_event'):
             s.click()
 
             s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/search_src_text')
 
-        with self.capture('user_search'):
+        with self.mark('user_search'):
             s.send_keys("Babej")
 
             s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/from')
 
-        with self.capture('conversation_open'):
+        with self.mark('conversation_open'):
             s.click()
 
             s = self.driver.find_element_by_id('org.thoughtcrime.securesms:id/embedded_text_editor')
 
-        with self.capture('send_regular_message'):
+        with self.mark('send_regular_message'):
             s.click()
             s.send_keys("This is an automated message. Please keep calm.\n")
             self.driver.find_element_by_id('org.thoughtcrime.securesms:id/send_button').click()
