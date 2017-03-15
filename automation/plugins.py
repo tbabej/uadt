6a1b6ca73edd5989d7acfa03dd95396ee08c9a62
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:37:56 +0100

    automation: Provide automated pcap logging abilities

--- automation/plugins.py
+++ automation/plugins.py
@@ -1,3 +1,8 @@
+import contextlib
+import subprocess
+import shlex
+import os
+
 from logger import LoggerMixin
 
 class PluginMount(type):
@@ -14,3 +19,18 @@ class PluginMount(type):
 class Plugin(LoggerMixin):
 
     __metaclass__ = PluginMount
+
+    @contextlib.contextmanager
+    def capture(self, name):
+        filename = '{plugin_identifier}_{name}_{timestamp}.pcap'.format({
+            'plugin_identifier': self.identifier,
+            'name': name,
+            'timestamp': datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        })
+
+        args = shlex.split("tshark -l -n -T pdml -i wlp4s0 -w {0}".format(filename))
+
+        with open(os.devnull, 'w') as f:
+            p = subprocess.Popen(args, stdout=f, stderr=f)
+            yield
+            p.terminate()
