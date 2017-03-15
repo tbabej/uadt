5a270014ff420fc0ab96f002d61bef8fc45fb388
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:36:57 +0100

    replicate: svm: Refactor the classification script

--- svm.py
+++ svm.py
@@ -1,41 +1,69 @@
-from sklearn import svm
+from sklearn import svm, model_selection, preprocessing
 import pandas
-import numpy
 
-origdf = pandas.read_csv('dataset10000.csv').dropna()
-origdf = origdf.sample(frac=1)
+class Machine(object):
 
-df = origdf.copy()
-results = df['class']
-df = df.drop('class', 1)
+    def __init__(self, path, train_size):
+        """
+        Initialize SVM machinery giving it the dataset at path to crunch.
+        """
 
-data = (df - df.mean()) / (df.max() - df.min())
-splitpoint = int(len(data)*0.8)
+        self.path = path
+        self.train_size = train_size
 
-X_train = data[:splitpoint].as_matrix()
-Y_train = results[:splitpoint].as_matrix()
+    def prepare_data(self):
+        """
+        Reads, randomizes data from the given dataset and splits it into test
+        and training sets. Test data set is not used during training or
+        parameter optimization.
+        """
 
-X_test = data[splitpoint:].as_matrix()
-Y_test = results[splitpoint:].as_matrix()
+        data = pandas.read_csv(self.path).dropna()
 
-C_candidates = [2.0**(2*p-1) for p in range(-2, 8)]
-gamma_candidates = [2.0**(2*p-1) for p in range(-8, 2)]
+        X = data.drop('class', 1)
+        y = data['class']
 
-results = []
-for C in C_candidates:
-    for gamma in gamma_candidates:
-        print "Testing C: {0}, gamma: {1}".format(C, gamma)
-        classifier = svm.SVC(C=C, gamma=gamma, decision_function_shape='ovo')
-        model = classifier.fit(X_train, Y_train)
-        prediction = model.predict(X_test)
-        compare_vector =  prediction - Y_test
-        #print prediction
-        #print Y_test
-        #print
-        #print compare_vector
+        splitted = model_selection.train_test_split(X, y, train_size=self.train_size)
 
-        rate = 100*len([p for p in compare_vector if p == 0])/float(len(compare_vector))
-        results.append((rate, C, gamma))
-        print rate, '% success'
+        # Convert to numpy arrays and scale inputs to [0,1] range
+        self.X_train = self.scale_array(splitted[0].as_matrix())
+        self.X_test  = self.scale_array(splitted[1].as_matrix())
+        self.y_train = splitted[2].as_matrix()
+        self.y_test  = splitted[3].as_matrix()
 
-print max(results)
+    @staticmethod
+    def scale_array(array):
+        """
+        Scales given array to [0,1] range.
+        """
+        scaler = preprocessing.MinMaxScaler()
+        return scaler.fit_transform(array)
+
+    def optimize_paramters(self):
+        """
+        Optimizes C and gamma parameters.
+        """
+
+        C_candidates = [2.0**(2*p-1) for p in range(-2, 9)]
+        gamma_candidates = [2.0**(2*p-1) for p in range(-8, 3)]
+
+        results = []
+        for C in C_candidates:
+            for gamma in gamma_candidates:
+                print "Testing C: {0}, gamma: {1}".format(C, gamma)
+                classifier = svm.SVC(C=C, gamma=gamma, decision_function_shape='ovo')
+                model = classifier.fit(self.X_train, self.y_train)
+                prediction = model.predict(self.X_test)
+                compare_vector =  prediction - self.y_test
+
+                rate = 100*len([p for p in compare_vector if p == 0])/float(len(compare_vector))
+                results.append((rate, C, gamma))
+                print rate, '% success'
+
+        _, self.C, self.gamma = max(results)
+
+
+machine = Machine("dataset1000.csv", train_size=0.7)
+machine.prepare_data()
+machine.optimize_paramters()
+print machine.C, machine.gamma
