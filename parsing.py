21d06a623eedb017c475929fd64be3df12cee22d
Tomas Babej <tomasbabej@gmail.com>
2017-03-15 10:36:45 +0100

    replicate: Flow: Add size and time features for backward traffic

--- parsing.py
+++ parsing.py
@@ -123,6 +123,66 @@ class Flow(object):
         """
         return self.forward_packets['timeshift'].std()
 
+    def feature_b_num(self):
+        """
+        Returns the number of backward packets.
+        """
+        return len(self.backward_packets)
+
+    def feature_b_size_sum(self):
+        """
+        Returns the total size of all backward packets.
+        """
+        return self.backward_packets['size'].sum()
+
+    def feature_b_size_min(self):
+        """
+        Returns the size of the smallest backward packet.
+        """
+        return self.backward_packets['size'].min()
+
+    def feature_b_size_max(self):
+        """
+        Returns the size of the biggest backward packet.
+        """
+        return self.backward_packets['size'].max()
+
+    def feature_b_size_mean(self):
+        """
+        Returns the mean size of the backward packets.
+        """
+        return self.backward_packets['size'].mean()
+
+    def feature_b_size_std(self):
+        """
+        Returns the mean size of the backward packets.
+        """
+        return self.backward_packets['size'].std()
+
+    def feature_b_time_min(self):
+        """
+        Returns the smallest inter time difference among backward packets.
+        """
+        return self.backward_packets['timeshift'].min()
+
+    def feature_b_time_max(self):
+        """
+        Returns the biggest inter time difference among backward packets.
+        """
+        return self.backward_packets['timeshift'].max()
+
+    def feature_b_time_mean(self):
+        """
+        Returns the mean inter time difference among backward packets.
+        """
+        return self.backward_packets['timeshift'].mean()
+
+    def feature_b_time_std(self):
+        """
+        Returns the std of inter time difference among backward packets.
+        """
+        return self.backward_packets['timeshift'].std()
+
 
 f = Flow(filename)
 print(f.generate_features())
