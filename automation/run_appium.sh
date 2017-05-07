fcb3bf5f55961bc5d63345b000372438fae7a604
Tomas Babej <tomasbabej@gmail.com>
2017-05-07 21:55:59 +0200

    automation: Harden appium setup

--- automation/run_appium.sh
+++ automation/run_appium.sh
@@ -12,6 +12,20 @@ start_appium () {
     sleep 3
   fi
 
+  if [[ ! -z `$ANDROID_HOME/platform-tools/adb devices | grep 'no permissions'` ]]
+  then
+    sudo $ANDROID_HOME/platform-tools/adb kill-server
+    sleep 2
+    sudo $ANDROID_HOME/platform-tools/adb start-server
+    sleep 2
+  fi
+
+  if [[ -z `$ANDROID_HOME/platform-tools/adb devices | grep 'device'` ]]
+  then
+    echo "Unable to get ADB working"
+    return
+  fi
+
   # Run appium only if appium-doctor checks pass
   if [[ ! -z `appium-doctor 2>&1 | tee /dev/stderr | grep 'Everything looks good'` ]]
   then
