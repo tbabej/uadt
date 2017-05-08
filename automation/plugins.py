b835a76f0b69c305b25c9e7784305e13b1d46061
Tomas Babej <tomasbabej@gmail.com>
2017-05-08 17:34:01 +0200

    plugins: Move necessary functionality into the metaclass

--- automation/plugins.py
+++ automation/plugins.py
@@ -2,39 +2,43 @@ from logger import LoggerMixin
 
 
 class PluginMount(type):
+    """
+    Provides common functionality for plugin base classes.
+    """
 
     def __init__(cls, name, bases, attrs):
         super(PluginMount, cls).__init__(name, bases, attrs)
 
-        if not hasattr(cls, 'plugins'):
-            cls.plugins = []
+        if not hasattr(cls, '_plugins'):
+            cls._plugins = []
         else:
-            cls.plugins.append(cls)
-
-
-class PluginBase(LoggerMixin):
-    """
-    Provides common functionality for plugin base classes. Each plugin base,
-    however, must furthermore specify the PluginMount metaclass.
-    """
+            cls._plugins.append(cls)
 
     @property
-    def plugins(self):
+    def plugins(cls):
         """
         Returns a dictionary of plugins available.
         """
 
         return {
             plugin_class.identifier: plugin_class
-            for plugin_class in self.plugins
+            for plugin_class in cls._plugins
         }
 
-    def get_plugin(self, identifier):
+
+class PluginBase(LoggerMixin):
+    """
+    Provides common functionality for plugin base classes. Each PluginBase
+    class still needs to specify PluginMount metaclass.
+    """
+
+    @classmethod
+    def get_plugin(cls, identifier):
         """
         Returns a plugin class corresponding to the given identifier.
         """
 
         try:
-            return self.plugins[identifier]
+            return cls.plugins[identifier]
         except KeyError:
-            self.error('Plugin "{0}" is not available'.format(identifier))
+            cls.error('Plugin "{0}" is not available'.format(identifier))
