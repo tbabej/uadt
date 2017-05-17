b0923341962b0fd60350322078252237647d0d80
Tomas Babej <tomasbabej@gmail.com>
2017-05-17 15:46:42 +0200

    signal: Port to new step based execution schema

--- uadt/automation/scenarios/signal.py
+++ uadt/automation/scenarios/signal.py
@@ -29,26 +29,52 @@ class SignalScenario(Scenario):
         (whether he's online or offline).
         """
 
-        def delivered_suffix(event_identifier):
-            return event_identifier + ('_delivered' if delivered else '_undelivered')
+        # TODO: Run two random walks here, changing the offline/online user status between
+        pass
+
+    @staticmethod
+    def delivered_suffix(event_identifier):
+        return event_identifier + ('_delivered' if self.delivered else '_undelivered')
+
+    def step_user_search(self):
+        """
+        Searches for the user in conversation list (main screen).
+
+        Start: main_screen
+        End:   filtered_screen
+        """
 
         s = self.find('org.thoughtcrime.securesms:id/menu_search')
         with self.mark('click_search_event'):
             self.click(s)
 
-        # User search
         s = self.find('org.thoughtcrime.securesms:id/search_src_text')
         with self.mark('user_search'):
             s.send_keys(user)
 
-        # Open conversation
+    def step_open_conversation(self):
+        """
+        Opens the conversation with the first user in the conversation list.
+        There should be only one, since this step is preceded by filtering by name.
+
+        Start: filtered_screen
+        End:   conversation
+        """
+
         s = self.find('org.thoughtcrime.securesms:id/from')
         with self.mark('conversation_open'):
             self.click(s)
 
-        # Send regular message
+    def step_send_regular_message(self):
+        """
+        Sends a message in the currently open conversation.
+
+        Start: conversation
+        End:   conversation
+        """
+
         s = self.find('org.thoughtcrime.securesms:id/embedded_text_editor')
-        with self.mark(delivered_suffix('send_regular_message')):
+        with self.mark(self.delivered_suffix('send_regular_message')):
             self.click(s)
             text = self.generator.text()
             self.add_metadata('message_length', len(text))
@@ -56,9 +82,16 @@ class SignalScenario(Scenario):
 
             self.click('org.thoughtcrime.securesms:id/send_button')
 
-        # Send picture
+    def step_send_picture(self):
+        """
+        Sends a randomly selected picture in the currently open conversation.
+
+        Start: conversation
+        End:   conversation
+        """
+
         s = self.find('org.thoughtcrime.securesms:id/attach_button')
-        with self.mark(delivered_suffix('send_image_no_caption')):
+        with self.mark(self.delivered_suffix('send_image_no_caption')):
             self.click(s)
             self.click(
                 "//android.widget.FrameLayout[@index='{0}']"
@@ -68,9 +101,16 @@ class SignalScenario(Scenario):
             self.click('org.thoughtcrime.securesms:id/send_button')
             time.sleep(3)  # Extra sleep for transfer
 
-        # Send location
+    def step_send_location(self):
+        """
+        Send current location in the currently open conversation.
+
+        Start: conversation
+        End:   conversation
+        """
+
         s = self.find('org.thoughtcrime.securesms:id/attach_button')
-        with self.mark(delivered_suffix('send_location')):
+        with self.mark(self.delivered_suffix('send_location')):
             self.click(s)
             self.click('org.thoughtcrime.securesms:id/location_button')
             self.click('com.google.android.gms:id/select_marker_location')
@@ -79,15 +119,30 @@ class SignalScenario(Scenario):
             self.click('org.thoughtcrime.securesms:id/send_button')
             time.sleep(3)  # Extra sleep for transfer
 
-        # Send GIF
+    def step_send_location(self):
+        """
+        Send the first offered GIF from GIPHY in the currently open conversation.
+
+        Start: conversation
+        End:   conversation
+        """
+
         s = self.find('org.thoughtcrime.securesms:id/attach_button')
-        with self.mark(delivered_suffix('send_gif')):
+        with self.mark(self.delivered_suffix('send_gif')):
             self.click(s)
             self.click('org.thoughtcrime.securesms:id/giphy_button')
             self.click('org.thoughtcrime.securesms:id/thumbnail')
 
             self.click('org.thoughtcrime.securesms:id/send_button')
             time.sleep(2)  # Extra sleep for transfer
+
+    def step_leave_conversation(self):
+        """
+        Send the first offered GIF from GIPHY in the currently open conversation.
+
+        Start: conversation
+        End:   main_screen
+        """
 
         # Press the back button (once to retract keyboard, once to go back)
         self.driver.press_keycode(4)
