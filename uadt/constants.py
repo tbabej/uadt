2634af6fcc41c6447b499ae7d70ef69b9fde838d
Tomas Babej <tomasbabej@gmail.com>
2017-05-18 13:59:05 +0000

    constants: Adjust class labels

--- uadt/constants.py
+++ uadt/constants.py
@@ -1,9 +1,13 @@
 CLASSES = {
         'click_search_event': 0,
-        'conversation_open': 1,
-        'send_gif': 2,
-        'send_image_no_caption': 3,
-        'send_location': 4,
-        'send_regular_message': 5,
-        'user_search': 6
+        'conversation_open': 0,
+        'send_gif_delivered': 1,
+        'send_image_no_caption_delivered': 2,
+        'send_location_delivered': 3,
+        'send_regular_message_delivered': 4,
+        'send_gif_undelivered': 5,
+        'send_image_no_caption_undelivered': 6,
+        'send_location_undelivered': 7,
+        'send_regular_message_undelivered': 8,
+        'user_search': 0
 }
